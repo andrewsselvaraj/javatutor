fc5f7aa8a62f3a9121ccfb318003c2a168101636
shiju <shiju@LAPTOP-QFQM3362>
2020-06-27 15:26:32 +0530

    First Commit

---
+++ javatutor/src/com/example/bean/Employee.java
@@ -0,0 +1,23 @@
+package com.example.bean;
+
+public class Employee {
+	
+	private double salary;
+	private int age;
+	private String name;
+
+	public Employee(String name,int age,double salary)
+	{
+		this.name=name;
+		this.age = age;
+		this.salary = salary;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
